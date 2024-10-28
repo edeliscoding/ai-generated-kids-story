e6af0b0ba0b1831a004c1a19258df44735b700fc
edeliscoding <edelreichd@gmail.com>
2024-10-28 10:45:57 -0600

    image next

--- app/components/storyTypeSelection.jsx
+++ app/components/storyTypeSelection.jsx
@@ -1,5 +1,6 @@
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import Image from "next/image";
 
 export default function ImageSelection({ imagevalue, imageonValueChange }) {
   return (
